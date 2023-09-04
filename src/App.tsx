9130f0b400094d796012726294b7e6db6d4c5500
kav310 <malimpatikavya@gmail.com>
2023-09-04 11:11:07 +0100

    intial app setup

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./styles/index.css";
+import {
+  BrowserRouter,
+  Route,
+  RouteComponentProps,
+  Switch
+} from "react-router-dom";
+
+interface IRoute {
+  path: string;
+  name: string;
+  exact: boolean;
+  component: any;
+  props?: any;
+}
+
+const App = () => {
+  const routes: IRoute[] = [
+    {
+      path: "/",
+      name: "Home Page",
+      component: "HomePage",
+      exact: true
+    },
+    {
+      path: "/about",
+      name: "About Page",
+      component: "AboutPage",
+      exact: true
+    }
+  ];
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          {routes.map((route, index) => {
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                render={(props: RouteComponentProps<any>) => (
+                  <route.component
+                    name={route.name}
+                    {...props}
+                    {...route.props}
+                  />
+                )}
+              />
+            );
+          })}
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Application from './application';
-import reportWebVitals from './reportWebVitals';
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+
+import App from "./App";
+
+import "./styles/tailwind.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Application />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root") as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
