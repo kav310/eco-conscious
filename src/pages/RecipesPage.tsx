43bb9569e8d9d1f17a42379d4a45324b5477278c
kav310 <malimpatikavya@gmail.com>
2023-09-05 01:00:45 +0100

    recipe selection by category

--- src/pages/RecipesPage.tsx
+++ src/pages/RecipesPage.tsx
@@ -14,19 +14,47 @@ export interface RecipesResponse {
 
 const RecipesPage = () => {
     const [recipesData, setRecipesData] = useState<RecipesResponse[] | undefined>(undefined);
+    const [categories, setCategories] = useState<any>(undefined);
     const history = useHistory()
+    const [selectedCategory, setSelectedCategory] = useState<string | undefined>(undefined)
 
     useEffect(() => {
-        axios.get(`http://localhost:5000/recipes`)
-            .then(response => setRecipesData(response.data)).catch(error => console.log(error))
+        if (selectedCategory && selectedCategory !== "Categories") {
+            axios.get(`http://localhost:5000/recipes/${selectedCategory}`)
+                .then(response => setRecipesData(response.data)).catch(error => console.log(error))
+        } else {
+            axios.get(`http://localhost:5000/recipes`)
+                .then(response => setRecipesData(response.data)).catch(error => console.log(error))
+        }
+    }, [selectedCategory])
+
+    useEffect(() => {
+        axios.get(`http://localhost:5000/categories`)
+            .then(response => setCategories(response.data)).catch(error => console.log(error))
     }, [])
 
     const handleSubmit = (id: string) => {
         history.push(`/recipe/${id}`)
     }
 
+    const handleChange = (e: any) => {
+        setSelectedCategory(e.target.value)
+    }
+
+
     return (
         <div className={"bg-gray-200"}>
+            <div className="flex justify-center pt-6">
+                <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sort
+                    By Categories</label>
+                <select id="countries" onChange={(e) => handleChange(e)}
+                        className=" max-w-md ml-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option selected>Categories</option>
+                    {categories && categories.map((item: any) => (
+                        <option value={item.name}>{item.name}</option>
+                    ))}
+                </select>
+            </div>
             <div className={"flex flex-row-reverse mr-6"}>
                 <button className="buttonTheme">Post Recipe</button>
             </div>
